b7e5d1ebb81d0a540b8304ce6082607a086383eb
bofh-hannibal <bofh@scd-systems.net>
2026-01-10 17:14:46 +0100

    chore: improve config loader
    Signed-off-by: bofh-hannibal <bofh@scd-systems.net>

--- configloader.go
+++ configloader.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	yaml "gopkg.in/yaml.v3"
 )
 
 func (config *ConfigFile) loadConfig(configFile string) error {
-	yamlFile, err := os.ReadFile(configFile)
-	if err == nil {
-		err = yaml.Unmarshal(yamlFile, config)
+	info, err := os.Stat(configFile)
+	if err != nil {
+		return fmt.Errorf("cannot access config file: %v", err)
 	}
-	return err
+
+	perms := info.Mode().Perm()
+	if perms&0077 != 0 {
+		return fmt.Errorf("config file has insecure permissions: %o (should be 0600)", perms)
+	}
+
+	yamlFile, err := os.ReadFile(configFile)
+	if err != nil {
+		return fmt.Errorf("cannot read config file: %v", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, config)
+	if err != nil {
+		return fmt.Errorf("cannot parse config file: %v", err)
+	}
+
+	return nil
 }
