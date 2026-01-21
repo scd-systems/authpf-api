1870c2f085626f7a7067b3a853616d43375e9bce
bofh-hannibal <bofh@scd-systems.net>
2026-01-21 17:28:05 +0100

    chore: fix logging

--- shutdown.go
+++ shutdown.go
@@ -8,9 +8,8 @@ import (
 )
 
 func gracefulShutdown(e *echo.Echo) error {
-	logger.Info().Msg("Deactivating all active authpf rules...")
-
 	if config.AuthPF.OnShutdown == "flushall" {
+		logger.Info().Msg("Deactivating all active authpf anchors...")
 		if err := deactivateAllActiveUsers(); err != nil {
 			logger.Error().Err(err).Msg("Error deactivating users")
 		}
@@ -30,19 +29,19 @@ func deactivateAllActiveUsers() error {
 	defer lock.Unlock()
 
 	if len(anchorsDB) == 0 {
-		logger.Info().Msg("No active authpf rules found")
+		logger.Info().Msg("No active authpf anchors found")
 		return nil
 	}
 
-	logger.Info().Int("count", len(anchorsDB)).Msg("Deactivating authpf rules")
+	logger.Info().Int("count", len(anchorsDB)).Msg("Deactivating authpf anchors")
 
 	// Create and exec pfctl flush for all authpf user rules
 	result := unloadAllAuthPFAnchors()
 	if result.Error != nil {
-		logger.Error().Err(result.Error).Msg("Error unloading pfctl rules")
+		logger.Error().Err(result.Error).Msg("Error unloading pfctl anchors")
 		return result.Error
 	}
 
-	logger.Info().Msg("All authpf rules deactivated successfully")
+	logger.Info().Msg("All authpf anchors deactivated successfully")
 	return nil
 }
